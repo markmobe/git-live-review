cf3503d6b0ec72bedf5381321c88074d40888dff
markmobe <153935790+markmobe@users.noreply.github.com>
2025-06-14 10:39:45 -0500

    Added divide method to simple calculator .java from GitHub

--- src/day02_pull_clone_rollback/SimpleCalculator.java
+++ src/day02_pull_clone_rollback/SimpleCalculator.java
@@ -13,6 +13,14 @@ public class SimpleCalculator {
     public static int multiply(int a, int b) {
         return a * b;
     }
+    
+    public static double divide(int a, int b) {
+        if (b == 0) {
+            System.out.println("Cannot divide by zero.");
+            return 0;
+    }
+    return (double) a / b;
+    }
 
     public static void main(String[] args) {
         int x = 10, y = 5;
