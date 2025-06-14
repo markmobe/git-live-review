3c10108c0cf57590dee38c3e75d99c5694094934
markmobe <markmobe.23@gmail.com>
2025-06-14 13:19:33 -0500

    Rool back updated

---
+++ src/day02_pull_clone_rollback/RollBackRevertPractice.java
@@ -0,0 +1,8 @@
+package day02_pull_clone_rollback;
+
+public class RollBackRevertPractice {
+
+    public static void main(String[] args) {
+        System.out.println("Git day 2 review");
+    }
+}
