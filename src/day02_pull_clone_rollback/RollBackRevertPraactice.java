9ed2216bc25911cbbd28d2fd54946978afb452ee
markmobe <markmobe.23@gmail.com>
2025-06-15 11:14:09 -0500

    Created RollBackRevert calss

--- src/day02_pull_clone_rollback/RollBackRevertPraactice.java
+++ src/day02_pull_clone_rollback/RollBackRevertPraactice.java
@@ -1,6 +1,6 @@
 package day02_pull_clone_rollback;
 
-public class RollBackRevertPractice {
+public class RollBackRevertPraactice {
 
     public static void main(String[] args) {
         System.out.println("Git day 2 review");
